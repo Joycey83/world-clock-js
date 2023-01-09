81f772173d5d631a07284e05c1d48ae052e4326f
Joycey <Jcheung8385@gmail.com>
2023-01-09 11:59:50 +0000

    Added moment.js date and time, added moment.js time zone CDN link to index.html. created querySelectors for all my default cities enabling me to use moment.js so I can access the date and local time of the city timezones

--- javascript/clock.js
+++ javascript/clock.js
@@ -1,1 +1,48 @@
+// Hong Kong Local Time Zone (Asia)
+let hongKong = document.querySelector("#hong-kong");
+let hongKongDate = hongKong.querySelector(".date");
+let hongKongTime = hongKong.querySelector(".time");
 
+let HongKongTimeElement = moment().tz("Asia/Hong_Kong");
+
+hongKongDate.innerHTML = HongKongTimeElement.format("Do MMMM YYYY");
+hongKongTime.innerHTML = HongKongTimeElement.format(
+  "h:mm:ss [<small>]A[</small>]"
+);
+
+// Louisville Local Time Zone (America)
+
+let louisville = document.querySelector("#louisville");
+let louisvilleDate = louisville.querySelector(".date");
+let louisvilleTime = louisville.querySelector(".time");
+
+let louisvilleTimeElement = moment().tz("America/Kentucky/Louisville");
+
+louisvilleDate.innerHTML = louisvilleTimeElement.format("Do MMMM YYYY");
+louisvilleTime.innerHTML = louisvilleTimeElement.format(
+  "h:mm:ss [<small>]A[</small>]"
+);
+
+// Rome Local Time Zone (Europe)
+
+let rome = document.querySelector("#rome");
+let romeDate = rome.querySelector(".date");
+let romeTime = rome.querySelector(".time");
+
+let romeTimeElement = moment().tz("Europe/Rome");
+
+romeDate.innerHTML = romeTimeElement.format("Do MMMM YYYY");
+romeTime.innerHTML = romeTimeElement.format("h:mm:ss [<small>]A[</small>]");
+
+// Johannesburg Local Time Zone (Africa)
+
+let johannesburg = document.querySelector("#johannesburg");
+let johannesburgDate = johannesburg.querySelector(".date");
+let johannesburgTime = johannesburg.querySelector(".time");
+
+let johannesburgTimeElement = moment().tz("Africa/Johannesburg");
+
+johannesburgDate.innerHTML = johannesburgTimeElement.format("Do MMMM YYYY");
+johannesburgTime.innerHTML = johannesburgTimeElement.format(
+  "h:mm:ss [<small>]A[</small>]"
+);
