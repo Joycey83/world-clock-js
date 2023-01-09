c36e8b65a248c1f3294ae4db161482c4cc765e98
Joycey <Jcheung8385@gmail.com>
2023-01-09 12:08:36 +0000

    Added a update Time City function so that the each second will be updated with each default city

--- javascript/clock.js
+++ javascript/clock.js
@@ -1,48 +1,52 @@
-// Hong Kong Local Time Zone (Asia)
-let hongKong = document.querySelector("#hong-kong");
-let hongKongDate = hongKong.querySelector(".date");
-let hongKongTime = hongKong.querySelector(".time");
+const updateCityTime = function () {
+  // Hong Kong Local Time Zone (Asia)
+  let hongKong = document.querySelector("#hong-kong");
+  let hongKongDate = hongKong.querySelector(".date");
+  let hongKongTime = hongKong.querySelector(".time");
 
-let HongKongTimeElement = moment().tz("Asia/Hong_Kong");
+  let HongKongTimeElement = moment().tz("Asia/Hong_Kong");
 
-hongKongDate.innerHTML = HongKongTimeElement.format("Do MMMM YYYY");
-hongKongTime.innerHTML = HongKongTimeElement.format(
-  "h:mm:ss [<small>]A[</small>]"
-);
+  hongKongDate.innerHTML = HongKongTimeElement.format("Do MMMM YYYY");
+  hongKongTime.innerHTML = HongKongTimeElement.format(
+    "H:mm:ss [<small>]A[</small>]"
+  );
 
-// Louisville Local Time Zone (America)
+  // Louisville Local Time Zone (America)
 
-let louisville = document.querySelector("#louisville");
-let louisvilleDate = louisville.querySelector(".date");
-let louisvilleTime = louisville.querySelector(".time");
+  let louisville = document.querySelector("#louisville");
+  let louisvilleDate = louisville.querySelector(".date");
+  let louisvilleTime = louisville.querySelector(".time");
 
-let louisvilleTimeElement = moment().tz("America/Kentucky/Louisville");
+  let louisvilleTimeElement = moment().tz("America/Kentucky/Louisville");
 
-louisvilleDate.innerHTML = louisvilleTimeElement.format("Do MMMM YYYY");
-louisvilleTime.innerHTML = louisvilleTimeElement.format(
-  "h:mm:ss [<small>]A[</small>]"
-);
+  louisvilleDate.innerHTML = louisvilleTimeElement.format("Do MMMM YYYY");
+  louisvilleTime.innerHTML = louisvilleTimeElement.format(
+    "H:mm:ss [<small>]A[</small>]"
+  );
 
-// Rome Local Time Zone (Europe)
+  // Rome Local Time Zone (Europe)
 
-let rome = document.querySelector("#rome");
-let romeDate = rome.querySelector(".date");
-let romeTime = rome.querySelector(".time");
+  let rome = document.querySelector("#rome");
+  let romeDate = rome.querySelector(".date");
+  let romeTime = rome.querySelector(".time");
 
-let romeTimeElement = moment().tz("Europe/Rome");
+  let romeTimeElement = moment().tz("Europe/Rome");
 
-romeDate.innerHTML = romeTimeElement.format("Do MMMM YYYY");
-romeTime.innerHTML = romeTimeElement.format("h:mm:ss [<small>]A[</small>]");
+  romeDate.innerHTML = romeTimeElement.format("Do MMMM YYYY");
+  romeTime.innerHTML = romeTimeElement.format("H:mm:ss [<small>]A[</small>]");
 
-// Johannesburg Local Time Zone (Africa)
+  // Johannesburg Local Time Zone (Africa)
 
-let johannesburg = document.querySelector("#johannesburg");
-let johannesburgDate = johannesburg.querySelector(".date");
-let johannesburgTime = johannesburg.querySelector(".time");
+  let johannesburg = document.querySelector("#johannesburg");
+  let johannesburgDate = johannesburg.querySelector(".date");
+  let johannesburgTime = johannesburg.querySelector(".time");
 
-let johannesburgTimeElement = moment().tz("Africa/Johannesburg");
+  let johannesburgTimeElement = moment().tz("Africa/Johannesburg");
 
-johannesburgDate.innerHTML = johannesburgTimeElement.format("Do MMMM YYYY");
-johannesburgTime.innerHTML = johannesburgTimeElement.format(
-  "h:mm:ss [<small>]A[</small>]"
-);
+  johannesburgDate.innerHTML = johannesburgTimeElement.format("Do MMMM YYYY");
+  johannesburgTime.innerHTML = johannesburgTimeElement.format(
+    "H:mm:ss [<small>]A[</small>]"
+  );
+};
+updateCityTime();
+setInterval(updateCityTime, 1000);
